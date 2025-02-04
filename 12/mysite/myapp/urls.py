2d1777d205aa73601a42b9f8e94d4a744b8c457b
Redrik <1423nikita@gmail.com>
2025-02-04 13:59:10 +0300

    12 13

--- 12/mysite/myapp/urls.py
+++ 12/mysite/myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # Главная страница
+    path('software/<int:software_id>/', views.software_detail, name='software_detail'),  # Детальный просмотр
+    path('software/<int:software_id>/edit/', views.software_edit, name='software_edit'),  # Редактирование
 ]
--- 12/mysite/myapp/views.py
+++ 12/mysite/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Software
 from .forms import SoftwareForm
 
@@ -16,3 +16,18 @@ def home(request):
         form = SoftwareForm()  # Пустая форма для GET-запроса
 
     return render(request, 'home.html', {'softwares': softwares, 'form': form})
+def software_detail(request, software_id):
+    software = get_object_or_404(Software, id=software_id)
+    return render(request, 'software_detail.html', {'software': software})
+def software_edit(request, software_id):
+    software = get_object_or_404(Software, id=software_id)
+    
+    if request.method == 'POST':
+        form = SoftwareForm(request.POST, instance=software)
+        if form.is_valid():
+            form.save()  # Сохраняем изменения
+            return redirect('software_detail', software_id=software.id)  # Перенаправляем на страницу с детальной информацией
+    else:
+        form = SoftwareForm(instance=software)  # Заполняем форму текущими данными записи
+
+    return render(request, 'software_edit.html', {'form': form, 'software': software})
